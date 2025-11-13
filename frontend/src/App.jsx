d6e33ca819910466e67f50f72f45c7fad5548674
Pabodavidumina123 <Pabodhavidumina@gmail.com>
2025-11-13 22:55:58 +0530

    added home page code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,6 @@ const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
       <Navbar/>
-
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/collection' element={<Collection/>} />
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar.jsx'
+import Hero from '../components/Hero.jsx'
 
 const home = () => {
   return (
     <div>
+      <Hero />
     </div>
   )
 }
