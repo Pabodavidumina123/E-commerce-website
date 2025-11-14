5791fe75596e403419f5f2d0148c5114a00622fe
Pabodavidumina123 <Pabodhavidumina@gmail.com>
2025-11-14 23:58:51 +0530

    creat thr home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar.jsx'
 import Hero from '../components/Hero.jsx'
+import LatestCollection from '../components/LatestCollection.jsx'
 
 const home = () => {
   return (
     <div>
       <Hero />
+      <LatestCollection/>
     </div>
   )
 }
